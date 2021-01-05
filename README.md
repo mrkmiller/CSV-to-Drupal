b36f555dc67081c5005bc3f92339f79c8e2a553c
Mark Miller <mrkmiller@ucdavis.edu>
2021-01-04 18:43:15 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+This is just some example code for taking a local CSV file and importing each row as a node into Drupal.
+
+1. Installation
+```
+npm install
+```
+
+2. Edit the index.js file with the CSV file location, Url, Username/Password, and any fields that should go into Drupal.
+
+3. Test the CSV parsing
+```
+npm test
+```
+
+4. Import the files into Drupal
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,102 @@
+const parse = require('csv-parse')
+const fs = require('fs')
+const axios = require('axios')
+
+// Edit the Url and Authentication values.
+const csvFile = `./example.csv`
+
+const url = 'http://d9.lndo.site:8080/jsonapi/node/page'
+
+const auth = {
+  username: 'admin',
+  password: 'admin'
+};
+
+/**
+ * Convert the CSV file into an array of data.
+ */
+const processFile = async () => {
+  records = []
+  const parser = fs
+    .createReadStream(csvFile)
+    .pipe(parse({
+      // CSV options if any https://csv.js.org/parse/options/
+    }))
+
+  for await (const record of parser) {
+    records.push(record)
+  }
+  return records
+}
+
+/**
+ * Convert the array of CSV data into Drupal data ready for JsonAPI.
+ * 
+ * @param {array} data
+ *   
+ */
+const prepareNodes = data => {
+  // Remove the first row as headers
+  data.shift()
+
+  const nodes = data.map(item => {
+    return {
+      data: {
+        type: 'node--page',
+        attributes: {
+          title: item[0],
+          body: {
+            value: `<p>${item[1]}</p>`,
+            format: 'basic_html'
+          }
+          // Add more fields here.
+        }
+      }
+    }
+  })
+
+  return nodes
+}
+
+/**
+ * Send a single Node into Drupal.
+ * 
+ * @param {object} data
+ *   The JsonAPI "data" object.
+ */
+const postToDrupal = async data => {
+  try {
+    return await axios({
+      method: 'post',
+      headers: {
+        'Accept': 'application/vnd.api+json',
+        'Content-Type': 'application/vnd.api+json',
+      },
+      url,
+      auth,
+      data,
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports.testFile = async function () {
+  const records = await processFile()
+  console.info(records)
+
+  const data = prepareNodes(records)
+  console.info(data)
+};
+
+module.exports.execute = async function () {
+  const records = await processFile()
+  const data = prepareNodes(records)
+
+  // Import the content into Drupal.
+  for await (const node of data) {
+    await postToDrupal(node)
+  }
+
+  console.log('All Done')
+}
